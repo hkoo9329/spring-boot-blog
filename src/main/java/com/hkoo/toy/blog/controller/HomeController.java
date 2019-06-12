77c243e9f695d518cd10b91ffd1fb5bc2d223289
leejuhyeok <tyrfingr12@naver.com>
2019-06-12 18:36:03 +0900

    기본 페이지에서 로그인 페이지로 이동 틀 구현

--- src/main/java/com/hkoo/toy/blog/controller/HomeController.java
+++ src/main/java/com/hkoo/toy/blog/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.hkoo.toy.blog.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String welcome(){
+    public String welcome(Model model){
+        model.addAttribute("name","lee");
         return "home";
     }
 }
