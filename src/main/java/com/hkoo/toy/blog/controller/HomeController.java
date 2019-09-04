8bd6e33773ce1afbb5774aa4343cce22c13fd55a
leejuhyeok <tyrfingr12@naver.com>
2019-09-04 19:44:37 +0900

    markdown 테스트를 위한 페이지와 mapping 추가

--- src/main/java/com/hkoo/toy/blog/controller/HomeController.java
+++ src/main/java/com/hkoo/toy/blog/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.hkoo.toy.blog.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
@@ -11,6 +12,13 @@ public class HomeController {
     @GetMapping("/")
     public String welcome(){
         return "home";
+    }
+
+
+    //임시 테스트
+    @RequestMapping("/test")
+    public String testPageMapping(){
+        return "testPage";
     }
 
 }
